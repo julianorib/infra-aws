25e6a50a8d1fdbfc0f3fe5e97ea4c65ef8a3decd
Juliano Ribeiro <julianorib@gmail.com>
2025-04-10 13:32:44 -0300

    Update nginx.sh

--- terraform/nginx.sh
+++ terraform/nginx.sh
@@ -2,6 +2,4 @@
 
 yum install nginx -y
 systemctl start nginx
-
-
-echo '<html><body><h1>Imagem do S3</h1><img src="https://julianorib-infra-aws.s3.us-east-1.amazonaws.com/bike" alt="Imagem"></body></html>' > /usr/share/nginx/html/index.html
+echo '<html><body><h1>Imagem do S3</h1><img src="https://julianorib-infra-aws.s3.us-west-1.amazonaws.com/bike2.jpg" alt="Imagem"></body></html>' > /usr/share/nginx/html/index.html
