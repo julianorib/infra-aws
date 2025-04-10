cfe7bb55708b9adea382eade97299e1c5dba5416
Juliano Ribeiro <julianorib@gmail.com>
2025-04-10 13:35:23 -0300

    branch testes infra-aws

--- terraform/4-instance.tf
+++ terraform/4-instance.tf
@@ -5,7 +5,8 @@ resource "aws_instance" "linux" {
   key_name        = aws_key_pair.main.key_name
   subnet_id       = data.aws_subnets.default.ids[0]
   security_groups = [aws_security_group.acesso-out-internet.id, aws_security_group.acesso-in-ssh.id, aws_security_group.acesso-in-web.id]
-
+  iam_instance_profile = aws_iam_instance_profile.ec2-s3-profile.name
+  
   tags = merge({ Name = format("%s-linux", var.project_name) }, local.common_tags)
 
 }
--- terraform/5-s3.tf
+++ terraform/5-s3.tf
@@ -1,8 +1,22 @@
 ## Cria um Bucket S3
 resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
-
   tags = merge({ Name = format("%s-bucket-s3", var.project_name) }, local.common_tags)
+}
+
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.main.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 ## Envia um arquivo para o Bucket S3
@@ -10,4 +24,11 @@ resource "aws_s3_object" "bike" {
   bucket = aws_s3_bucket.main.id
   key    = "bike.jpg"
   source = "bike.jpg"
+  acl = "public-read"
+}
+
+resource "aws_s3_object" "bike2" {
+  bucket = aws_s3_bucket.main.id
+  key    = "bike2.jpg"
+  source = "bike2.jpg"
 }
